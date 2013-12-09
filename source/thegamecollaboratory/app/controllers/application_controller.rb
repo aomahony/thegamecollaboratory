d72c044f6e8dea349f46d2ab8f6eb2f71d8e6b08
Andrew O'Mahony <aomahony@artlogic.com>
2013-12-09 00:18:58 -0800

    aomahony:Converted views to use HAML

--- source/thegamecollaboratory/app/controllers/application_controller.rb
+++ source/thegamecollaboratory/app/controllers/application_controller.rb
@@ -3,7 +3,16 @@ class ApplicationController < ActionController::Base
    # For APIs, you may want to use :null_session instead.
    protect_from_forgery with: :exception
 
+   before_filter :authenticate 
+
    rescue_from CanCan::AccessDenied do |exception|
       redirect_to main_app.root_path, :alert => exception.message
    end
+
+   private
+      def authenticate
+         if params[:controller] != 'landing' and params[:controller] != 'sessions'
+            redirect_to(main_app.root_path) unless current_user != nil and current_user.is_admin?
+         end
+      end
 end
--- source/thegamecollaboratory/app/models/admin_ability.rb
+++ source/thegamecollaboratory/app/models/admin_ability.rb
@@ -2,7 +2,7 @@ class AdminAbility
    include CanCan::Ability
 
    def initialize(user)
-      if true == user.is_admin?
+      if true == user.is_admin? and true == user.is_active?
          can :dashboard
          can :access, :rails_admin
          can :manage, :all 
